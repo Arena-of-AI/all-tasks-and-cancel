35d7373fd31e9ff5ea5a2961eb3951c3958f38b9
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-29 14:57:47 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import openai
+
+# 设置 OpenAI API 密钥输入框
+api_key = st.text_input("Enter your OpenAI API key")
+
+# 初始化 OpenAI
+openai.api_key = api_key
+
+# 列出所有 FineTune 作业
+def list_all_jobs():
+    response = openai.FineTune.list()
+    data = response["data"]
+
+    table_data = []
+    for item in data:
+        created_at = item["created_at"]
+        fine_tuned_model = item["fine_tuned_model"]
+        model = item["model"]
+        status = item["status"]
+        table_data.append((created_at, fine_tuned_model, model, status))
+
+    st.table(table_data)
+
+# 取消 FineTune 作业
+def cancel_job(job_id):
+    openai.FineTune.cancel(id=job_id)
+    st.success(f"Job with ID {job_id} has been canceled.")
+
+# 主应用
+def main():
+    st.title("OpenAI Fine-Tune Jobs")
+
+    # 显示列表按钮
+    if st.button("List All Jobs"):
+        list_all_jobs()
+
+    # 取消作业输入框和按钮
+    cancel_job_id = st.text_input("Enter the Job ID to cancel")
+    if st.button("Cancel Job") and cancel_job_id:
+        cancel_job(cancel_job_id)
+
+# 运行主应用
+if __name__ == "__main__":
+    main()
